db0fb3eaef760f5190c52062f9fae9399db296b6
Haris Anjum <hanjum6@macbookpro.home>
2024-11-10 20:01:14 -0500

    modifying the View to handle ingredient search

--- src/main/java/view/RecipeSearchView.java
+++ src/main/java/view/RecipeSearchView.java
@@ -1,95 +1,183 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-
-import interface_adapter.note.NoteController;
-import interface_adapter.note.NoteState;
-import interface_adapter.note.NoteViewModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 /**
- * The View for when the user is viewing a note in the program.
+ * A view for searching recipes by ingredients.
  */
-public class NoteView extends JPanel implements ActionListener, PropertyChangeListener {
+public class RecipeSearchView extends JPanel implements ActionListener, PropertyChangeListener {
 
-    private final NoteViewModel noteViewModel;
+    private static final int TEXTFIELD_WIDTH = 20;
+    private static final int VERTICAL_SPACING = 10;
+    private static final int INGREDIENT_LIST_HEIGHT = 100;
+    private static final int INGREDIENT_LIST_WIDTH = 300;
+    private static final int RESULTS_LIST_HEIGHT = 200;
+    private static final int RESULTS_LIST_WIDTH = 300;
+    private static final int TITLE_FONT_SIZE = 16;
 
-    private final JLabel noteName = new JLabel("note for jonathan_calver2");
-    private final JTextArea noteInputField = new JTextArea();
-
-    private final JButton saveButton = new JButton("Save");
-    private final JButton refreshButton = new JButton("Refresh");
-    private NoteController noteController;
-
-    public NoteView(NoteViewModel noteViewModel) {
-
-        noteName.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.noteViewModel = noteViewModel;
-        this.noteViewModel.addPropertyChangeListener(this);
-
-        final JPanel buttons = new JPanel();
-        buttons.add(saveButton);
-        buttons.add(refreshButton);
-
-        saveButton.addActionListener(
-                evt -> {
-                    if (evt.getSource().equals(saveButton)) {
-                        noteController.execute(noteInputField.getText());
-
-                    }
-                }
-        );
-
-        refreshButton.addActionListener(
-                evt -> {
-                    if (evt.getSource().equals(refreshButton)) {
-                        noteController.execute(null);
-
-                    }
-                }
-        );
-
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        this.add(noteName);
-        this.add(noteInputField);
-        this.add(buttons);
-    }
+    private final JLabel title;
+    private final JPanel inputPanel;
+    private final JTextField ingredientField;
+    private final JButton addIngredientButton;
+    private final DefaultListModel<String> ingredientListModel;
+    private final JList<String> ingredientList;
+    private final JButton removeIngredientButton;
+    private final JButton searchButton;
+    private final JPanel resultsPanel;
+    private final DefaultListModel<String> recipeListModel;
+    private final JList<String> recipeResults;
+    private final List<String> ingredients;
 
     /**
-     * React to a button click that results in evt.
-     * @param evt the ActionEvent to react to
+     * Constructs a new RecipeSearchView.
      */
+    public RecipeSearchView() {
+        // Initialize components
+        title = new JLabel("Recipe Search by Ingredients");
+        inputPanel = new JPanel();
+        ingredientField = new JTextField(TEXTFIELD_WIDTH);
+        addIngredientButton = new JButton("Add Ingredient");
+        ingredientListModel = new DefaultListModel<>();
+        ingredientList = new JList<>(ingredientListModel);
+        removeIngredientButton = new JButton("Remove Selected");
+        searchButton = new JButton("Search Recipes");
+        resultsPanel = new JPanel();
+        recipeListModel = new DefaultListModel<>();
+        recipeResults = new JList<>(recipeListModel);
+        ingredients = new ArrayList<>();
+
+        // Set up the main layout
+        this.setLayout(new BorderLayout());
+
+        // Configure the title
+        title.setFont(new Font(title.getFont().getName(), Font.BOLD, TITLE_FONT_SIZE));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Set up panels
+        setupInputPanel();
+        setupResultsPanel();
+
+        // Add action listeners
+        addIngredientButton.addActionListener(this);
+        removeIngredientButton.addActionListener(this);
+        searchButton.addActionListener(this);
+
+        // Main panel assembly
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(Box.createVerticalStrut(VERTICAL_SPACING));
+        mainPanel.add(title);
+        mainPanel.add(Box.createVerticalStrut(VERTICAL_SPACING));
+        mainPanel.add(inputPanel);
+        mainPanel.add(Box.createVerticalStrut(VERTICAL_SPACING));
+        mainPanel.add(searchButton);
+        mainPanel.add(Box.createVerticalStrut(VERTICAL_SPACING));
+        mainPanel.add(resultsPanel);
+
+        this.add(mainPanel, BorderLayout.CENTER);
+    }
+
+    private void setupInputPanel() {
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+
+        final JPanel addIngredientPanel = new JPanel();
+        addIngredientPanel.add(new JLabel("Enter Ingredient:"));
+        addIngredientPanel.add(ingredientField);
+        addIngredientPanel.add(addIngredientButton);
+
+        final JScrollPane ingredientScrollPane = new JScrollPane(ingredientList);
+        ingredientScrollPane.setPreferredSize(
+                new Dimension(INGREDIENT_LIST_WIDTH, INGREDIENT_LIST_HEIGHT));
+
+        final JPanel removeButtonPanel = new JPanel();
+        removeButtonPanel.add(removeIngredientButton);
+
+        inputPanel.add(addIngredientPanel);
+        inputPanel.add(Box.createVerticalStrut(VERTICAL_SPACING));
+        inputPanel.add(ingredientScrollPane);
+        inputPanel.add(removeButtonPanel);
+    }
+
+    private void setupResultsPanel() {
+        resultsPanel.setLayout(new BoxLayout(resultsPanel, BoxLayout.Y_AXIS));
+
+        final JLabel resultsLabel = new JLabel("Recipe Results:");
+        resultsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        final JScrollPane resultsScrollPane = new JScrollPane(recipeResults);
+        resultsScrollPane.setPreferredSize(
+                new Dimension(RESULTS_LIST_WIDTH, RESULTS_LIST_HEIGHT));
+
+        resultsPanel.add(resultsLabel);
+        resultsPanel.add(Box.createVerticalStrut(VERTICAL_SPACING));
+        resultsPanel.add(resultsScrollPane);
+    }
+
+    @Override
     public void actionPerformed(ActionEvent evt) {
-        System.out.println("Click " + evt.getActionCommand());
+        if (evt.getSource().equals(addIngredientButton)) {
+            final String ingredient = ingredientField.getText().trim();
+            if (!ingredient.isEmpty()) {
+                ingredients.add(ingredient);
+                ingredientListModel.addElement(ingredient);
+                ingredientField.setText("");
+            }
+        }
+        else if (evt.getSource().equals(removeIngredientButton)) {
+            final int selectedIndex = ingredientList.getSelectedIndex();
+            if (selectedIndex != -1) {
+                ingredients.remove(selectedIndex);
+                ingredientListModel.remove(selectedIndex);
+            }
+        }
+        else if (evt.getSource().equals(searchButton)) {
+            // TODO: Implement search functionality
+            // This will be connected to the controller
+            System.out.println("Searching for recipes with ingredients: " + ingredients);
+        }
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        final NoteState state = (NoteState) evt.getNewValue();
-        setFields(state);
-        if (state.getError() != null) {
-            JOptionPane.showMessageDialog(this, state.getError(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
+        // TODO: Handle property changes from the view model
+        // This will update the recipe results when the search is complete
+    }
+
+    /**
+     * Returns a copy of the current list of ingredients.
+     * @return List of ingredients
+     */
+    public List<String> getIngredients() {
+        return new ArrayList<>(ingredients);
+    }
+
+    /**
+     * Updates the recipe results display.
+     * @param recipes List of recipe names to display
+     */
+    public void updateRecipeResults(List<String> recipes) {
+        recipeListModel.clear();
+        for (String recipe : recipes) {
+            recipeListModel.addElement(recipe);
         }
     }
-
-    private void setFields(NoteState state) {
-        noteInputField.setText(state.getNote());
-    }
-
-    public void setNoteController(NoteController controller) {
-        this.noteController = controller;
-    }
 }
-
