769c826cc6b39e29c48ea10c9bd43d65ce6a2e1d
Haris Anjum <hanjum6@MacBook-Pro-469.local>
2024-11-18 18:57:56 -0500

    Meal Planning Output Boundary

--- src/main/java/use_case/meal_planning/MealPlanningOutputBoundary.java
+++ src/main/java/use_case/meal_planning/MealPlanningOutputBoundary.java
@@ -1,4 +1,14 @@
 package use_case.meal_planning;
 
-public class MealPlanningOutputBoundary {
+import entity.MealPlanEntry;
+import entity.Recipe;
+import java.util.List;
+
+public interface MealPlanningOutputBoundary {
+    void presentCalendarWeek(List<MealPlanEntry> entries);
+    void presentAddSuccess(String message);
+    void presentRemoveSuccess(String message);
+    void presentStatusUpdateSuccess(String message);
+    void presentError(String error);
+    void presentSavedRecipes(List<Recipe> recipes);
 }
